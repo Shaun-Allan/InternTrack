35c0523d68f4b34c59e480dfdaee37e6ab1c6420
Shaun-Allan <shaunallan0605@gmail.com>
2025-02-23 19:14:12 +0530

    hello

--- student/student.js
+++ student/student.js
@@ -34,4 +34,36 @@ function initialize() {
       uploadText.classList.add("file-name");
     }
   });
+
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbw_3K5a0D_ILostm2py5ZG2iQ7KIGcJPHYaFVervjr9bdrAtB3l0DrFtRSfdH0dOBfQ/exec";
+  const form = document.querySelector("#intern-form");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Convert FormData to an Object for logging
+    let formData = new FormData(form);
+    let formObject = Object.fromEntries(formData.entries());
+    console.log("Form Data:", formObject);
+
+    try {
+      let response = await fetch(scriptURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formObject).toString(),
+      });
+
+      let result = await response.text();
+      console.log("Response from Server:", result);
+
+      alert("Thanks! Your form has been submitted.");
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  });
+}
+
+function logout() {
+  window.location.href = "../index.html";
 }
